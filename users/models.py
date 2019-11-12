494b02b9a233e8289044d0d1634e07e008dc21cb
Davis Too <davis.too@strathmore.edu>
2019-11-12 21:05:53 +0300

    made user return an integer

--- users/models.py
+++ users/models.py
@@ -23,7 +23,7 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.id
+        return self.username
 
 
 def add_to_default_group(sender, instance, **kwargs):
